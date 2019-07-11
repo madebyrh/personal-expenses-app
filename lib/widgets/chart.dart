2c772ddf79eafaad038d57bc6f441584754ce0d4
madebyrh <madebyrh@gmail.com>
2019-07-11 23:21:26 +0900

    fix layout

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -28,7 +28,7 @@ class Chart extends StatelessWidget {
         'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
-    });
+    }).reversed.toList(); //reversedメソッドでchart barが右から、左からが変わる
   }
 
   double get totalSpending {
